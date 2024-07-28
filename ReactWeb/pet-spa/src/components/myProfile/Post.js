2232badb474d72d015472c58a75bd8bd4ed177ab
PolinaRomanchuk <polinar_99@mail.ru>
2024-07-28 13:12:46 +0300

    add photo switching

--- ReactWeb/pet-spa/src/components/myProfile/Post.js
+++ ReactWeb/pet-spa/src/components/myProfile/Post.js
@@ -5,18 +5,30 @@ import OpenPost from './openPost';
 
 function Post({ model, userPhoto, fullmodel }) {
     const [active, setActive] = useState(false);
+    const [currentIndex, setCurrentIndex] = useState(fullmodel.indexOf(model));
 
+    const prevPost = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % fullmodel.length);
+    };
+
+    const nextPost = () => {
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + fullmodel.length) % fullmodel.length);
+    };
 
     return (
         <>
             <div className="post-item" >
-               
-                     <img src={model.imageUrl} onClick={() => setActive(true)} />
-                    
-
+                <img src={model.imageUrl} onClick={() => setActive(true)} />
             </div>
             <Modal active={active} setActive={setActive}>
-                <OpenPost active={active} setActive={setActive} model={model} userPhoto={userPhoto} fullmodel={fullmodel} />
+                <OpenPost
+                    active={active}
+                    setActive={setActive}
+                    model={fullmodel[currentIndex]}
+                    userPhoto={userPhoto}
+                    fullmodel={fullmodel}
+                    nextPost={nextPost}
+                    prevPost={prevPost} />
             </Modal>
         </>
     );
--- ReactWeb/pet-spa/src/components/myProfile/openPost.js
+++ ReactWeb/pet-spa/src/components/myProfile/openPost.js
@@ -2,15 +2,18 @@ import React, { useContext, useState } from "react";
 import { AuthContext } from '../login/auth';
 import './openPost.css';
 import Comment from "./Comment";
+import { ArrowLeft, ArrowRight } from 'lucide-react';
 
-function OpenPost({ model, setActive, userPhoto, fullmodel }) {
+function OpenPost({ model, setActive, userPhoto, fullmodel, active, nextPost, prevPost, }) {
     const { userId, name } = useContext(AuthContext);
+
     const closeWindow = () => {
         setActive(false);
     }
 
     return (
-        <>
+        <div className="open-post-main-container">
+            <button className="btn-paginator-arrow left" onClick={prevPost}> <ArrowLeft /></button>
             <div className="open-post-container">
                 <button className="close-button" onClick={closeWindow} />
                 <div className="selected-photo-container">
@@ -34,7 +37,8 @@ function OpenPost({ model, setActive, userPhoto, fullmodel }) {
                     </div>
                 </div>
             </div>
-        </>
+            <button className="btn-paginator-arrow right" onClick={nextPost}><ArrowRight /></button>
+        </div>
     );
 };
 
